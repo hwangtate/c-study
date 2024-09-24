0b8ebdf0a533bbcdec48bbf33832f3ec32e1338c
hwangtate <ghkdxodud10253504@gmail.com>
2024-09-24 19:52:03 +0900

    int, char, printf, scanf

--- hello.c
+++ hello.c
@@ -2,6 +2,28 @@
 
 int main(void)
 {
-    printf("hello world\n");
-    return 0;
+    // 정수 변수
+    // int age = 12;
+    // printf("%d", age);
+
+    // 실수 변수
+    // float age = 12.4f;
+    // printf("%.2f", age);
+    // return 0;
+
+    // 정수 상수
+    // const int YEAR = 2000;
+    // printf("태어난 벌스 : %d\n", YEAR);
+
+    // printf 계산
+    // printf("%d + %d = %d\n", 3, 7, 3 + 10);
+
+    // int input;
+    // printf("값을 입력하세요: ");
+    // scanf("%d", &input);
+    // printf("입력 값: %d\n", input);
+    // return 0;
+
+    char c = 'A';
+    printf("%c\n", c);
 }
